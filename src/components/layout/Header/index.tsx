58076a991cef733bc8c69464630faf07ee3c66ef
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-20 12:53:48 +0300

    chore: header component style include a border

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 import NavBar from "./Navbar"
 
 const Header: FC = () => {
-  return <header className="fixed top-0 left-0 right-0 z-30 backdrop-blur-sm shadow-lg p-2 lg:px-8 bg-tuatara-50 dark:bg-tuatara-1000 bg-opacity-90 dark:bg-opacity-90">
+  return <header className="fixed top-0 left-0 right-0 z-30 backdrop-blur-sm p-2 lg:px-8 bg-tuatara-50 dark:bg-tuatara-1000 bg-opacity-90 dark:bg-opacity-90 border-b border-tuatara-100 dark:border-tuatara-900">
     <div className="max-w-screen-3xl flex justify-between items-center mx-auto">
       <Logo />
 
