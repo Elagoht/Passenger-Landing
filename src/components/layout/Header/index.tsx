16b26e085fbf249f92abf59b741abf982d323286
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-20 14:33:08 +0300

    feat: Add Hamburger component for mobile menu

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react"
 import Logo from "./Logo"
 import NavBar from "./Navbar"
+import Hamburger from "./Hamburger"
 
-const Header: FC = () => {
-  return <header className="fixed top-0 left-0 right-0 z-30 backdrop-blur-sm p-2 lg:px-8 bg-tuatara-50 dark:bg-tuatara-1000 bg-opacity-90 dark:bg-opacity-90 border-b border-tuatara-100 dark:border-tuatara-900">
+const Header: FC = () =>
+  <header className="fixed top-0 left-0 right-0 z-30 backdrop-blur-sm p-2 lg:px-8 bg-tuatara-50 dark:bg-tuatara-1000 bg-opacity-90 dark:bg-opacity-90 border-b border-tuatara-100 dark:border-tuatara-900">
     <div className="max-w-screen-3xl flex justify-between items-center mx-auto">
       <Logo />
 
       <NavBar />
+
+      <Hamburger />
     </div>
   </header>
-}
 
 export default Header
--- src/components/pages/main/Hero/index.tsx
+++ src/components/pages/main/Hero/index.tsx
@@ -4,8 +4,8 @@ import HeroSlider from "./HeroSlider"
 import HeroText from "./HeroText"
 
 const Hero: FC = () =>
-  <main className="lg:px-24 flex lg:gap-24 p-4 items-center justify-center max-w-screen-3xl mx-auto max-lg:flex-col">
-    <article className="flex flex-col max-lg:my-24 basis-2/5 prose dark:prose-invert prose-xl">
+  <main className="lg:px-24 flex lg:gap-24 gap-4 p-4 items-center justify-center max-w-screen-3xl mx-auto max-lg:flex-col">
+    <article className="flex flex-col max-lg:my-4 basis-2/5 prose dark:prose-invert prose-xl">
       <HeroText />
 
       <CTAButton />
