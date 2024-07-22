e54914516f895080b0cc67bbe43547673e41a151
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-23 00:25:36 +0300

    feat: Update DocsLayout to include a main container for content

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -9,7 +9,9 @@ const DocsLayout: FC<IDocsLayoutProps> = ({ children }) =>
   <div className="max-w-screen-2xl flex flex-1 w-full self-center">
     <DocsAside />
 
-    {children}
+    <main className="md:p-8 p-4 bg-tuatara-100 dark:bg-tuatara-950">
+      {children}
+    </main>
   </div>
 
 export default DocsLayout
