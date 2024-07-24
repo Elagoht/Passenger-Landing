ce5067469b0bb50ab1f74cdf3ee806fd80f2e3bf
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-24 23:57:35 +0300

    feat: Add 404 Not Found page for documentation

---
+++ src/app/docs/not-found.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+
+const DocsNotFoundPage: FC = () => <main>
+  <h1>
+    404 Not Found
+  </h1>
+
+  <p>
+    The page you are looking for does not exist. Please check the URL or go back to the
+    <a href="/docs">Docs</a>.
+  </p>
+</main>
+
+export default DocsNotFoundPage
