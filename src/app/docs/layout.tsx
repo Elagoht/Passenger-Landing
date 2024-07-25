ae5cef3af8450212defb5bbc8eb61991c4899d3e
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-07-26 00:00:44 +0300

    chore: Update layout to stretch main content and add styling to 404 page

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -9,7 +9,7 @@ const DocsLayout: FC<IDocsLayoutProps> = ({ children }) =>
   <div className="max-w-screen-2xl flex flex-1 w-full self-center items-start">
     <DocsAside />
 
-    <main className="md:p-8 p-4 bg-tuatara-100 dark:bg-tuatara-950">
+    <main className="md:p-8 p-4 bg-tuatara-100 dark:bg-tuatara-950 self-stretch">
       {children}
     </main>
   </div>
--- src/app/docs/not-found.tsx
+++ src/app/docs/not-found.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react"
+import "@/design/article.css"
 
-const DocsNotFoundPage: FC = () => <main>
-  <h1>
-    404 Not Found
+const DocsNotFoundPage: FC = () => <article className="h-full text-center flex items-center justify-center flex-col">
+  <h1 className="mb-0">
+    404: Not Found
   </h1>
+
+  <hr />
 
   <p>
     The page you are looking for does not exist. Please check the URL or go back to the
     <a href="/docs">Docs</a>.
   </p>
-</main>
+</article>
 
 export default DocsNotFoundPage
